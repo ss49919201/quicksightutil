e5bfb086dc66b8d12e297970a3e3de770c47ee3c
ss49919201 <sakashin10291029@gmail.com>
2023-08-22 23:28:08 +0900

    Add command to copy data set

---
+++ cmd/cp-data-set/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/quicksight"
+)
+
+func main() {
+	accountID := flag.String("account-id", "", "AWS Account ID")
+	region := flag.String("region", "", "AWS Region ID")
+	srcID := flag.String("src-id", "", "Source DataSet ID")
+	dstID := flag.String("dst-id", "", "Destination DataSet ID")
+
+	flag.Parse()
+
+	ctx := context.Background()
+	cfg, err := config.LoadDefaultConfig(
+		ctx,
+		config.WithRegion(aws.ToString(region)),
+	)
+	if err != nil {
+		log.Printf("unable to load SDK config, %v", err)
+		os.Exit(1)
+	}
+
+	client := quicksight.NewFromConfig(cfg)
+	describeOutput, err := client.DescribeDataSet(ctx, &quicksight.DescribeDataSetInput{
+		AwsAccountId: accountID,
+		DataSetId:    srcID,
+	})
+	if err != nil {
+		log.Printf("unable to describe data set, %v", err)
+		os.Exit(1)
+	}
+
+	describePermissionOutput, err := client.DescribeDataSetPermissions(ctx, &quicksight.DescribeDataSetPermissionsInput{
+		AwsAccountId: accountID,
+		DataSetId:    srcID,
+	})
+	if err != nil {
+		log.Printf("unable to describe data set permissions, %v", err)
+		os.Exit(1)
+	}
+
+	// TODO
+	// describeDataSetRefreshPropertiesOutput, err := client.DescribeDataSetRefreshProperties(ctx, &quicksight.DescribeDataSetRefreshPropertiesInput{
+	// 	AwsAccountId: accountID,
+	// 	DataSetId:    srcID,
+	// })
+	// if err != nil {
+	// 	log.Printf("unable to describe data set refresh properties, %v", err)
+	// 	os.Exit(1)
+	// }
+
+	if _, err := client.CreateDataSet(ctx, &quicksight.CreateDataSetInput{
+		AwsAccountId:                       accountID,
+		DataSetId:                          dstID,
+		ImportMode:                         describeOutput.DataSet.ImportMode,
+		Name:                               dstID,
+		PhysicalTableMap:                   describeOutput.DataSet.PhysicalTableMap,
+		ColumnGroups:                       describeOutput.DataSet.ColumnGroups,
+		ColumnLevelPermissionRules:         describeOutput.DataSet.ColumnLevelPermissionRules,
+		DataSetUsageConfiguration:          describeOutput.DataSet.DataSetUsageConfiguration,
+		DatasetParameters:                  describeOutput.DataSet.DatasetParameters,
+		FieldFolders:                       describeOutput.DataSet.FieldFolders,
+		LogicalTableMap:                    describeOutput.DataSet.LogicalTableMap,
+		Permissions:                        describePermissionOutput.Permissions,
+		RowLevelPermissionDataSet:          describeOutput.DataSet.RowLevelPermissionDataSet,
+		RowLevelPermissionTagConfiguration: describeOutput.DataSet.RowLevelPermissionTagConfiguration,
+		// Tags: TODO,
+	}); err != nil {
+		log.Printf("unable to describe data set refresh properties, %v", err)
+		os.Exit(1)
+	}
+
+	// TODO
+	// if _, err := client.PutDataSetRefreshProperties(ctx, &quicksight.PutDataSetRefreshPropertiesInput{
+	// 	AwsAccountId:             accountID,
+	// 	DataSetId:                dstID,
+	// 	DataSetRefreshProperties: describeDataSetRefreshPropertiesOutput.DataSetRefreshProperties,
+	// }); err != nil {
+	// 	log.Printf("unable to describe data set refresh properties, %v", err)
+	// 	os.Exit(1)
+	// }
+}
